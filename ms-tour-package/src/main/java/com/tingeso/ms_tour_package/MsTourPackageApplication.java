1b1f0774b18ef1b274b6d2af381da67ae2d8579b
benya <benjamin.sandoval.b@usach.cl>
2026-07-08 00:55:08 -0400

    Estructura inicial: agregando servicios de infraestructura y backends

---
+++ ms-tour-package/src/main/java/com/tingeso/ms_tour_package/MsTourPackageApplication.java
@@ -0,0 +1,13 @@
+package com.tingeso.ms_tour_package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTourPackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTourPackageApplication.class, args);
+	}
+
+}
---
+++ ms-tour-package/src/test/java/com/tingeso/ms_tour_package/MsTourPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingeso.ms_tour_package;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTourPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
